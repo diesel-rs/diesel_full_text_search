5d794381394819f6277a00a0cfb427878fe59953
Dario Cancelliere <dario.cancelliere@prima.it>
2021-02-08 16:56:30 +0100

    Configuration type with all needed constants

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,14 @@
 extern crate diesel;
 
 mod types {
+    use std::io::Write;
+
+    use diesel::backend::Backend;
+    use diesel::deserialize::{self, FromSql};
+    use diesel::serialize::{self, Output};
+    use diesel::sql_types::Integer;
+    use diesel::types::ToSql;
+
     #[allow(deprecated)]
     use diesel::SqlType;
 
@@ -14,9 +22,50 @@ mod types {
     pub struct TsVector;
     pub type Tsvector = TsVector;
 
+    pub const CONFIGURATION_SIMPLE: u32 = 3748;
+    pub const CONFIGURATION_DANISH: u32 = 12824;
+    pub const CONFIGURATION_DUTCH: u32 = 12826;
+    pub const CONFIGURATION_ENGLISH: u32 = 12828;
+    pub const CONFIGURATION_FINNISH: u32 = 12830;
+    pub const CONFIGURATION_FRENCH: u32 = 12832;
+    pub const CONFIGURATION_GERMAN: u32 = 12834;
+    pub const CONFIGURATION_HUNGARIAN: u32 = 12836;
+    pub const CONFIGURATION_ITALIAN: u32 = 12838;
+    pub const CONFIGURATION_NORWEGIAN: u32 = 12840;
+    pub const CONFIGURATION_PORTUGUESE: u32 = 12842;
+    pub const CONFIGURATION_ROMANIAN: u32 = 12844;
+    pub const CONFIGURATION_RUSSIAN: u32 = 12846;
+    pub const CONFIGURATION_SPANISH: u32 = 12848;
+    pub const CONFIGURATION_SWEDISH: u32 = 12850;
+    pub const CONFIGURATION_TURKISH: u32 = 12852;
+
     #[derive(SqlType)]
     #[postgres(type_name = "regconfig")]
     pub struct Regconfig;
+
+    #[derive(Debug, PartialEq, AsExpression)]
+    #[sql_type = "Regconfig"]
+    pub struct TsConfiguration(u32);
+
+    impl<DB> FromSql<Regconfig, DB> for TsConfiguration
+    where
+        DB: Backend,
+        i32: FromSql<Integer, DB>,
+    {
+        fn from_sql(bytes: Option<&DB::RawValue>) -> deserialize::Result<Self> {
+            <i32 as FromSql<Integer, DB>>::from_sql(bytes).map(|oid| TsConfiguration(oid as u32))
+        }
+    }
+
+    impl<DB> ToSql<Regconfig, DB> for TsConfiguration
+    where
+        DB: Backend,
+        i32: ToSql<Integer, DB>,
+    {
+        fn to_sql<W: Write>(&self, out: &mut Output<W, DB>) -> serialize::Result {
+            <i32 as ToSql<Integer, DB>>::to_sql(&(*&self.0 as i32), out)
+        }
+    }
 }
 
 #[allow(deprecated)]
