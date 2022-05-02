311d0692a9fa92fbfe86a0bed15663bda9514dea
Dario Cancelliere <dario.cancelliere@prima.it>
2022-05-02 17:52:45 +0200

    Upgrade to Diesel 2.0 RC

--- src/lib.rs
+++ src/lib.rs
@@ -8,11 +8,11 @@ mod types {
     use diesel::SqlType;
 
     #[derive(Clone, Copy, SqlType)]
-    #[postgres(oid = "3615", array_oid = "3645")]
+    #[diesel(postgres_type(oid = 3615, array_oid = 3645))]
     pub struct TsQuery;
 
     #[derive(Clone, Copy, SqlType)]
-    #[postgres(oid = "3614", array_oid = "3643")]
+    #[diesel(postgres_type(oid = 3614, array_oid = 3643))]
     pub struct TsVector;
     pub type Tsvector = TsVector;
 
@@ -22,24 +22,22 @@ mod types {
     impl TextOrNullableText for Nullable<Text> {}
 
     #[derive(SqlType)]
-    #[postgres(type_name = "regconfig")]
-    pub struct Regconfig;
+    #[diesel(postgres_type(name = "regconfig"))]
+    pub struct RegConfig;
 }
 
 pub mod configuration {
-    use crate::Regconfig;
-
-    use std::io::Write;
+    use crate::RegConfig;
 
     use diesel::backend::Backend;
+    use diesel::backend::RawValue;
     use diesel::deserialize::{self, FromSql};
-    use diesel::serialize::{self, Output};
+    use diesel::serialize::{self, Output, ToSql};
     use diesel::sql_types::Integer;
-    use diesel::types::ToSql;
 
     #[derive(Debug, PartialEq, AsExpression)]
-    #[sql_type = "Regconfig"]
-    pub struct TsConfiguration(pub u32);
+    #[diesel(sql_type = RegConfig)]
+    pub struct TsConfiguration(pub i32);
 
     impl TsConfiguration {
         pub const SIMPLE: Self = Self(3748);
@@ -60,23 +58,23 @@ pub mod configuration {
         pub const TURKISH: Self = Self(12852);
     }
 
-    impl<DB> FromSql<Regconfig, DB> for TsConfiguration
+    impl<DB> FromSql<RegConfig, DB> for TsConfiguration
     where
         DB: Backend,
         i32: FromSql<Integer, DB>,
     {
-        fn from_sql(bytes: Option<&DB::RawValue>) -> deserialize::Result<Self> {
-            <i32 as FromSql<Integer, DB>>::from_sql(bytes).map(|oid| TsConfiguration(oid as u32))
+        fn from_sql(bytes: RawValue<'_, DB>) -> deserialize::Result<Self> {
+            i32::from_sql(bytes).map(|oid| TsConfiguration(oid))
         }
     }
 
-    impl<DB> ToSql<Regconfig, DB> for TsConfiguration
+    impl<DB> ToSql<RegConfig, DB> for TsConfiguration
     where
         DB: Backend,
         i32: ToSql<Integer, DB>,
     {
-        fn to_sql<W: Write>(&self, out: &mut Output<W, DB>) -> serialize::Result {
-            <i32 as ToSql<Integer, DB>>::to_sql(&(*&self.0 as i32), out)
+        fn to_sql<'b>(&'b self, out: &mut Output<'b, '_, DB>) -> serialize::Result {
+            self.0.to_sql(out)
         }
     }
 }
@@ -94,12 +92,12 @@ mod functions {
     sql_function!(fn to_tsquery(x: Text) -> TsQuery);
     sql_function! {
         #[sql_name = "to_tsquery"]
-        fn to_tsquery_with_search_config(config: Regconfig, querytext: Text) -> TsQuery;
+        fn to_tsquery_with_search_config(config: RegConfig, querytext: Text) -> TsQuery;
     }
-    sql_function!(fn to_tsvector<T: TextOrNullableText>(x: T) -> TsVector);
+    sql_function!(fn to_tsvector<T: TextOrNullableText + SingleValue>(x: T) -> TsVector);
     sql_function! {
         #[sql_name = "to_tsvector"]
-        fn to_tsvector_with_search_config<T: TextOrNullableText>(config: Regconfig, document_content: T) -> TsVector;
+        fn to_tsvector_with_search_config<T: TextOrNullableText + SingleValue>(config: RegConfig, document_content: T) -> TsVector;
     }
     sql_function!(fn ts_headline(x: Text, y: TsQuery) -> Text);
     sql_function!(fn ts_rank(x: TsVector, y: TsQuery) -> Float);
@@ -113,7 +111,6 @@ mod functions {
 }
 
 mod dsl {
-    use diesel::expression::grouped::Grouped;
     use diesel::expression::{AsExpression, Expression};
     use types::*;
 
@@ -121,17 +118,15 @@ mod dsl {
         use diesel::pg::Pg;
         use types::*;
 
-        diesel_infix_operator!(Matches, " @@ ", backend: Pg);
-        diesel_infix_operator!(Concat, " || ", TsVector, backend: Pg);
-        diesel_infix_operator!(And, " && ", TsQuery, backend: Pg);
-        diesel_infix_operator!(Or, " || ", TsQuery, backend: Pg);
-        diesel_infix_operator!(Contains, " @> ", backend: Pg);
-        diesel_infix_operator!(ContainedBy, " <@ ", backend: Pg);
+        diesel::infix_operator!(Matches, " @@ ", backend: Pg);
+        diesel::infix_operator!(Concat, " || ", TsVector, backend: Pg);
+        diesel::infix_operator!(And, " && ", TsQuery, backend: Pg);
+        diesel::infix_operator!(Or, " || ", TsQuery, backend: Pg);
+        diesel::infix_operator!(Contains, " @> ", backend: Pg);
+        diesel::infix_operator!(ContainedBy, " <@ ", backend: Pg);
     }
 
     use self::predicates::*;
-
-    pub type Concat<T, U> = Grouped<predicates::Concat<T, U>>;
 
     pub trait TsVectorExtensions: Expression<SqlType = TsVector> + Sized {
         fn matches<T: AsExpression<TsQuery>>(self, other: T) -> Matches<Self, T::Expression> {
@@ -139,7 +134,7 @@ mod dsl {
         }
 
         fn concat<T: AsExpression<TsVector>>(self, other: T) -> Concat<Self, T::Expression> {
-            Grouped(predicates::Concat::new(self, other.as_expression()))
+            Concat::new(self, other.as_expression())
         }
     }
 
